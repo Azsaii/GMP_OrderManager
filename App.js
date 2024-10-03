818d8d2faadc90a1735ce8478e8afb09ed7a7fd2
azsaii <socczx@naver.com>
2024-10-03 19:25:12 +0900

    Feat: 매출 통계 기능

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import OrderManagement from './screens/OrderManagement';
+import Statistics from './screens/Statistics';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="주문 관리" component={OrderManagement} />
+        <Tab.Screen name="통계" component={Statistics} />
       </Tab.Navigator>
     </NavigationContainer>
   );
